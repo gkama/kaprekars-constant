e9a7373b89a9f960217d67ac00804c320541ef5e
Georgi Kamacharov <gkamacharov@gmail.com>
2022-02-03 13:01:23 -0500

    update to string?

--- kaprekars.constant.data/Routine.cs
+++ kaprekars.constant.data/Routine.cs
@@ -2,10 +2,10 @@
 {
     public class Routine
     {
-        public string Number { get; set; }
-        public string Ascending { get; set; }
-        public string Descending { get; set; }
-        public string Result { get; set; }
+        public string? Number { get; set; }
+        public string? Ascending { get; set; }
+        public string? Descending { get; set; }
+        public string? Result { get; set; }
         public string? Subtraction { get; set; }
     }
 }
